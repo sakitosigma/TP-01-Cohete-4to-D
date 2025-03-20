b475d2da106fbfdfddbd159f694e2a177e3f8c54
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2025-03-20 10:40:32 -0300

    Boton funcionando

---
+++ Assets/Scripts/BotonScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotonScript : MonoBehaviour
+{
+    public GameObject botonGO;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        botonGO.SetActive(false);
+    }
+
+    void OnMouseUp()
+    {
+        botonGO.SetActive(true);
+    }
+}
